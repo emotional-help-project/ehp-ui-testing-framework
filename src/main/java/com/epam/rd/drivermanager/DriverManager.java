e4832ac887716ba0ea6c0990ad15060566e49246
Dan Postolache <dan_postolache@epam.com>
2022-11-28 19:29:35 +0200

    implemented DriverManager, BasePage, DriverHooks

--- src/main/java/com/epam/rd/drivermanager/DriverManager.java
+++ src/main/java/com/epam/rd/drivermanager/DriverManager.java
@@ -1,4 +1,30 @@
 package com.epam.rd.drivermanager;
 
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import java.util.Optional;
+
 public class DriverManager {
+    private static final ThreadLocal<WebDriver> webDriverThreadLocal = new ThreadLocal<>();
+
+    private DriverManager() {
+    }
+
+    public static void setupDriver() {
+        WebDriverManager.chromedriver().setup();
+        WebDriver driver = new ChromeDriver();
+        driver.manage().window().maximize();
+        webDriverThreadLocal.set(driver);
+    }
+
+    public static WebDriver getDriver() {
+        return webDriverThreadLocal.get();
+    }
+
+    public static void quitDriver() {
+        Optional.ofNullable(getDriver()).ifPresent(WebDriver::quit);
+        webDriverThreadLocal.remove();
+    }
 }
--- src/main/java/com/epam/rd/pageobjects/BasePage.java
+++ src/main/java/com/epam/rd/pageobjects/BasePage.java
@@ -1,4 +1,15 @@
 package com.epam.rd.pageobjects;
 
+import com.epam.rd.drivermanager.DriverManager;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.support.PageFactory;
+import org.openqa.selenium.support.pagefactory.AjaxElementLocatorFactory;
+
 public class BasePage {
+    protected WebDriver driver;
+
+    protected BasePage() {
+        driver = DriverManager.getDriver();
+        PageFactory.initElements(new AjaxElementLocatorFactory(driver, 5), this);
+    }
 }
